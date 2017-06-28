c0224c44c7f844b4f237a08c4c5f7c05d3cf04b7
Charlie Wang <s455wang@csclub.uwaterloo.ca>
2017-06-28 12:11:12 -0400

    use static frame from bit-shifter instead of stupid wget/sed

--- openoffice.sh
+++ openoffice.sh
@@ -1,26 +1,13 @@
 #! /bin/sh
 
-outputfile=$(mktemp -p .)
-frame=$(mktemp -p .)
-
-cleanup () {
-    rm $outputfile
-    rm $frame
-}
-trap cleanup exit
+frame=$HOME/bit-shifter-webcam/frame.jpg
 
 main () {
-    timeout 2.5s wget -N -q http://bit-shifter:8081/ -O $outputfile
-    set -e
-
     # guard against empty file
-    if [ ! -s $outputfile ]; then
+    if [ ! -s $frame ]; then
         echo "Could not fetch webcam stream." 1>&2
         exit 5
     fi
-
-    boundary=$(awk '/--BoundaryString/{ print NR; }' ${outputfile} | sed -n 2p)
-    sed -n 5,${boundary}p ${outputfile} | head -n -1 > $frame
 
     brightness=$(convert ${frame} -colorspace gray -format  "%[fx:floor(100*mean)]" info:)
 
