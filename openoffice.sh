a7b0ad7b4508ff82e2d795eef02140d980e0bd24
Charlie Wang <s455wang@csclub.uwaterloo.ca>
2017-08-08 09:38:21 -0400

    Revert "use static frame from bit-shifter instead of stupid wget/sed"
    This didn't actually work
    
    This reverts commit c0224c44c7f844b4f237a08c4c5f7c05d3cf04b7.

--- openoffice.sh
+++ openoffice.sh
@@ -1,13 +1,26 @@
 #! /bin/sh
 
-frame=$HOME/bit-shifter-webcam/frame.jpg
+outputfile=$(mktemp -p .)
+frame=$(mktemp -p .)
+
+cleanup () {
+    rm $outputfile
+    rm $frame
+}
+trap cleanup exit
 
 main () {
+    timeout 2.5s wget -N -q http://bit-shifter:8081/ -O $outputfile
+    set -e
+
     # guard against empty file
-    if [ ! -s $frame ]; then
+    if [ ! -s $outputfile ]; then
         echo "Could not fetch webcam stream." 1>&2
         exit 5
     fi
+
+    boundary=$(awk '/--BoundaryString/{ print NR; }' ${outputfile} | sed -n 2p)
+    sed -n 5,${boundary}p ${outputfile} | head -n -1 > $frame
 
     brightness=$(convert ${frame} -colorspace gray -format  "%[fx:floor(100*mean)]" info:)
 
